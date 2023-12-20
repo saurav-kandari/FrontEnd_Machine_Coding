e2df8c5ad4aca2af1f1e09d1ebbedaaddc3d23b0
Saurav Kandari <sauravkandari@Sauravs-MacBook-Air.local>
2023-12-20 14:01:36 +0530

    added otp-input

---
+++ otp-input/script.js
@@ -0,0 +1,28 @@
+const inputs = document.getElementById("input-container") ;
+
+inputs.addEventListener("input",(e)=>{
+  let target = e.target ;
+  if(target.value != ""){
+    let value = parseInt(target.value) ;
+    console.log(value) ;
+    if(isNaN(value)){
+      target.value = "" ;
+    }else{
+      let next = target.nextElementSibling ;
+      if(next){
+        next.focus() ;
+      }
+    }
+  }
+}) ;
+
+inputs.addEventListener("keyup", (e)=>{
+  let target = e.target ;
+  let key = e.key.toLowerCase() ;
+  if(key == 'backspace' || key == 'delete'){
+    let prev = target.previousElementSibling ;
+    if(prev){
+      prev.focus() ;
+    }
+  }
+});
